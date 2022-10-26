6b3e0134f1fc3fc38ad432ca1be7063025b7ff41
cleisommais <cleisommais@gmail.com>
2022-10-25 22:14:52 -0500

    Init project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Learning and practicing Algorithms and Design Patterns
+
+-   Node.js
+-   Husky
+-   Chai
+-   Mocha
+-   Prettier
+-   Eslint
---
+++ src/arrays/two-number-sum.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param {Number[]} array
+ * @param {Number} targetSum
+ * @returns
+ */
+function twoNumberSum(array = [], targetSum = 0) {
+    // Write your code here.
+    return true;
+}
+
+// Do not edit the line below.
+exports.twoNumberSum = twoNumberSum;
