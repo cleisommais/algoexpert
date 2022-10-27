e06c4340d44acc691590f541ad11fcc927ae0d7a
cleisommais <cleisommais@gmail.com>
2022-10-26 19:55:53 -0500

    Two Number Sum - using Set with one loop

--- src/arrays/two-number-sum.js
+++ src/arrays/two-number-sum.js
@@ -2,20 +2,16 @@
  * Time: O(n) | Space O(n)
  * @param {Number[]} array
  * @param {Number} targetSum
- * @returns
+ * @returns {Number[]}
  */
 function twoNumberSum(array = [], targetSum = 0) {
-    const map = new Map();
+    const set = new Set();
     for (let i = 0; i < array.length; i++) {
         const diff = targetSum - array[i];
-        map.set(diff, array[i]);
-    }
-    for (let i = 0; i < array.length; i++) {
-        if (map.has(array[i])) {
-            if (map.get(array[i]) !== array[i]) {
-                return [map.get(array[i]), array[i]];
-            }
+        if (set.has(diff)) {
+            return [array[i], diff];
         }
+        set.add(array[i]);
     }
     return [];
 }
