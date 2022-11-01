38fca83ad76ff8d6aa156c9ebf9cb754d7be3e7c
cleisommais <cleisommais@gmail.com>
2022-11-01 06:17:19 -0500

    Changed the Arrays - Longest Peak

--- test/arrays/longest-peak.test.js
+++ test/arrays/longest-peak.test.js
@@ -2,7 +2,10 @@ const program = require('../../src/arrays/longest-peak');
 const chai = require('chai');
 
 it('Arrays - Longest Peak', function () {
-    const array = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3];
-    const expected = 6;
-    chai.expect(program.longestPeak(array)).to.deep.equal(expected);
+    chai.expect(
+        program.longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3])
+    ).to.deep.equal(6);
+    chai.expect(
+        program.longestPeak([5, 4, 3, 2, 1, 2, 10, 12, -3, 5, 6, 7, 10])
+    ).to.deep.equal(5);
 });
